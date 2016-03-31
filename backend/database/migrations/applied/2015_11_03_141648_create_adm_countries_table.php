d1955576213c5ac51c3c25f5aa4f5a9634421fb3
Terian | Erick Christian Rosales Cruz <erick.rosales@terian.com.mx>
2016-03-31 14:34:03 -0600

    Actualización de Aplicación 31 de Marzo del 2016.

---
+++ backend/database/migrations/applied/2015_11_03_141648_create_adm_countries_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdmCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Table Countries
+        Schema::create('adm_countries' , function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->char('country_name');
+
+            /* Enable Soft Delete*/
+            $table->softDeletes();
+
+            /* Indexes */
+            $table->unique('id');
+            $table->index('country_name');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //Delete Table
+        Schema::drop('adm_countries');
+    }
+}
+
